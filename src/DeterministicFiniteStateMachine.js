198dd38b85a73a5a0a9e2d48b28c4d4ea63feb5f
Mike Megally <cmsimike@gmail.com>
2020-02-17 16:54:38 -0800

    all tests pass. might take another pass at cleanup

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -35,22 +35,100 @@ export default class DeterministicFiniteStateMachine {
 /**
  *
  */
+ /*
+     DeterministicFiniteStateMachine {
+      transitions: {
+        r0: { '0': 'r0', '1': 'r1' },
+        r1: { '0': 'r2', '1': 'r0' },
+        r2: { '0': 'r1', '1': 'r2' }
+      },
+      startState: 'r0',
+      acceptStates: [ 'r0' ]
+    }
+*/
 export function cross(dfa1, dfa2, acceptanceCriteria = (dfa1State, dfa2State) => true) {
-//.... TODO
-  return dfa1;
+ //console.warn(dfa1);
+  //console.warn(dfa2);
+  // final info
+  let newTransitions = {};
+  let startState = '';
+  let acceptStates = [];
+
+  // work variables
+
+  let dfa1Transitions = dfa1['transitions'];
+  let dfa2Transitions = dfa2['transitions'];
+  let dfa1StateKeys = Object.keys(dfa1Transitions);
+  //console.warn(dfa1StateKeys);
+  let dfa2StateKeys = Object.keys(dfa2Transitions);
+  //console.warn(dfa2StateKeys);
+
+  let newStates = [];
+
+  // Generate all new states
+  for(let i = 0; i < dfa1StateKeys.length; i++) {
+    for(let j = 0; j < dfa2StateKeys.length; j++) {
+      // console.warn(dfa1StateKeys[i] + '-' + dfa2StateKeys[j])
+      let newStateName = dfa1StateKeys[i] + '-' + dfa2StateKeys[j];
+
+      newStates.push(newStateName);
+    }
+  }
+
+  // for each of the new states, let's figure out the new transitions
+  for(let i = 0; i < newStates.length; i++) {
+    // we know the new states are stored in <dfa1>-<dfa2> format, so 
+    // we can just split on '-'
+    let splitStates = newStates[i].split('-');
+    // console.warn(splitStates);
+    let dfa1TransitionToStateOn0 = dfa1['transitions'][splitStates[0]]['0'];
+    //console.warn("dfa1TransitionToStateOn0 " +  dfa1TransitionToStateOn0);
+    let dfa2TransitionToStateOn0 = dfa2['transitions'][splitStates[1]]['0'];
+    //console.warn("dfa2TransitionToStateOn0 " +  dfa2TransitionToStateOn0);
+    let dfa1TransitionToStateOn1 = dfa1['transitions'][splitStates[0]]['1'];
+    //console.warn("dfa1TransitionToStateOn1 " +  dfa1TransitionToStateOn1);
+    let dfa2TransitionToStateOn1 = dfa2['transitions'][splitStates[1]]['1'];
+    //console.warn("dfa2TransitionToStateOn1 " +  dfa2TransitionToStateOn1);
+    newTransitions[newStates[i]] = {};
+    newTransitions[newStates[i]]['0'] = dfa1TransitionToStateOn0 + '-' + dfa2TransitionToStateOn0;
+    newTransitions[newStates[i]]['1'] = dfa1TransitionToStateOn1 + '-' + dfa2TransitionToStateOn1;
+  }
+  // figure out new start state
+  startState = dfa1['startState'] + '-' + dfa2['startState'];
+
+  // figure out new accept states
+  for(let i = 0; i < newStates.length; i++) {
+    // we know the new states are stored in <dfa1>-<dfa2> format, so 
+    // we can just split on '-'
+    let states = newStates[i].split('-');
+    // console.log(states[0] + "|||||" + states[1]);
+    if (acceptanceCriteria(states[0], states[1])) {
+       acceptStates.push(newStates[i]);
+    }
+  }
+
+  // build the object
+  let dfaCross = {
+      transitions: newTransitions,
+      startState: startState,
+      acceptStates: acceptStates,
+    };
+  console.warn(dfaCross);
+
+  return new DeterministicFiniteStateMachine (dfaCross);
 }
 
 export function union(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
+    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) || dfa2.stateAccepted(dfa2State));
 }
 
 export function intersection(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
+    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) && dfa2.stateAccepted(dfa2State));
 }
 
 export function minus(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
+    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) && !dfa2.stateAccepted(dfa2State));
 }
