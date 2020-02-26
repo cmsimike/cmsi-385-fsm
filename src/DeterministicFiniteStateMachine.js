c1f2fb9b88ea8d97b3abc3239a25266d985f0251
Mike Megally <cmsimike@gmail.com>
2020-02-26 14:58:10 -0800

    hw6 programming

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -1,113 +1,234 @@
 export default class DeterministicFiniteStateMachine {
 
-  /**
-   */
-  constructor({ transitions, startState, acceptStates }) {
-    this.transitions = transitions;
-    this.startState = startState;
-    this.acceptStates = acceptStates;
-  }
+    /**
+     */
+    constructor({transitions, startState, acceptStates}) {
+        this.transitions = transitions;
+        this.startState = startState;
+        this.acceptStates = acceptStates;
+    }
 
-  states() {
-    return Object.keys(this.transitions);
-  }
+    alphabet() {
+        const alphabet = new Set();
 
-  stateAccepted(state) {
-    return this.acceptStates.includes(state);
-  }
+        for (const [state, desc] of Object.entries(this.transitions)) {
+            for (const symbol of Object.keys(desc)) {
+                alphabet.add(symbol);
+            }
+        }
 
-  /**
-   *
-   * @returns a string state name
-   */
-  transition(state, symbol) {
-    return this.transitions[state][symbol];
-  }
+        return alphabet.values();
+    }
 
-  accepts(string, state = this.startState) {
-    const nextState = this.transition(state, string.charAt(0));
-    return (string.length === 0) ? this.stateAccepted(state) :
-                                   this.accepts(string.substr(1), nextState);
-  }
+    states() {
+        return new Set(Object.keys(this.transitions));
+    }
+
+    stateAccepted(state) {
+        return this.acceptStates.includes(state);
+    }
+
+    /**
+     *
+     * @returns a string state name
+     */
+    transition(state, symbol) {
+        if (!this.transitions[state]) return;
+        return this.transitions[state][symbol];
+    }
+
+    accepts(string, state = this.startState) {
+        const nextState = this.transition(state, string.charAt(0));
+        return (string.length === 0) ? this.stateAccepted(state) :
+            this.accepts(string.substr(1), nextState);
+    }
 
 }
 
 /**
-*/
-export function cross(dfa1, dfa2, acceptanceCriteria = (dfa1State, dfa2State) => true) {
-  // final info
-  let newTransitions = {};
-  let startState = '';
-  let acceptStates = [];
+ *
+ */
+export function cross(dfa1, dfa2, accepts = (dfa1State, dfa2State) => true) {
+    const acceptStates = [];
+    const transitions = {};
+    const alphabet = new Set([...dfa1.alphabet(), ...dfa2.alphabet()]);
 
-  // work variables
-  let dfa1Transitions = dfa1['transitions'];
-  let dfa2Transitions = dfa2['transitions'];
+    // A function which returns a state name for a state in machine 1 and a state in machine 2
+    const stateName = (state1, state2) => `m1:${state1}xm2:${state2}`;
 
-  let dfa1StateKeys = Object.keys(dfa1Transitions);
-  let dfa2StateKeys = Object.keys(dfa2Transitions);
+    const startState = stateName(dfa1.startState, dfa2.startState);
+    const unresolvedStates = [{state: startState, state1: dfa1.startState, state2: dfa2.startState}];
 
-  let newStates = [];
+    while (unresolvedStates.length > 0) {
+        const {state1, state2, state} = unresolvedStates.pop();
 
-  // Generate all new states
-  for(let i = 0; i < dfa1StateKeys.length; i++) {
-    for(let j = 0; j < dfa2StateKeys.length; j++) {
-      let newStateName = dfa1StateKeys[i] + '-' + dfa2StateKeys[j];
+        transitions[state] = {};
+        if (accepts(state1, state2)) acceptStates.push(state);
 
-      newStates.push(newStateName);
+        for (const symbol of alphabet) {
+            const nextState1 = dfa1.transition(state1, symbol);
+            const nextState2 = dfa2.transition(state2, symbol);
+
+            const nextState = stateName(nextState1, nextState2);
+            transitions[state][symbol] = nextState;
+
+            if (!transitions[nextState]) {
+                // recording that we need to process this state
+                unresolvedStates.push({state: nextState, state1: nextState1, state2: nextState2});
+            }
+        }
     }
-  }
 
-  // for each of the new states, let's figure out the new transitions
-  for(let i = 0; i < newStates.length; i++) {
-    // we know the new states are stored in <dfa1>-<dfa2> format, so 
-    // we can just split on '-'
-    let splitStates = newStates[i].split('-');
-
-    let dfa1TransitionToStateOn0 = dfa1['transitions'][splitStates[0]]['0'];
-    let dfa2TransitionToStateOn0 = dfa2['transitions'][splitStates[1]]['0'];
-
-    let dfa1TransitionToStateOn1 = dfa1['transitions'][splitStates[0]]['1'];
-    let dfa2TransitionToStateOn1 = dfa2['transitions'][splitStates[1]]['1'];
-
-    newTransitions[newStates[i]] = {};
-    newTransitions[newStates[i]]['0'] = dfa1TransitionToStateOn0 + '-' + dfa2TransitionToStateOn0;
-    newTransitions[newStates[i]]['1'] = dfa1TransitionToStateOn1 + '-' + dfa2TransitionToStateOn1;
-  }
-  // figure out new start state
-  startState = dfa1['startState'] + '-' + dfa2['startState'];
-
-  // figure out new accept states
-  for(let i = 0; i < newStates.length; i++) {
-    // we know the new states are stored in <dfa1>-<dfa2> format, so 
-    // we can just split on '-'
-    let states = newStates[i].split('-');
-    if (acceptanceCriteria(states[0], states[1])) {
-       acceptStates.push(newStates[i]);
-    }
-  }
-
-  // build the object
-  let dfaCross = {
-      transitions: newTransitions,
-      startState: startState,
-      acceptStates: acceptStates,
-    };
-
-  return new DeterministicFiniteStateMachine (dfaCross);
+    return new DeterministicFiniteStateMachine({
+        acceptStates,
+        startState,
+        transitions
+    });
 }
 
 export function union(dfa1, dfa2) {
-  return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) || dfa2.stateAccepted(dfa2State));
+    return cross(dfa1, dfa2,
+        (state1, state2) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
 }
 
 export function intersection(dfa1, dfa2) {
-  return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) && dfa2.stateAccepted(dfa2State));
+    return cross(dfa1, dfa2,
+        (state1, state2) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
 }
 
 export function minus(dfa1, dfa2) {
-  return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(dfa1State) && !dfa2.stateAccepted(dfa2State));
+    return cross(dfa1, dfa2,
+        (state1, state2) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
+}
+
+export function minimize(dfa) {
+    function constructGroupObj() {
+        return {
+            'acceptStates': [],
+            'queryStates': [],
+            'individualAcceptStates': [],
+            'individualStates': []
+        }
+    }
+
+    let stateNameGen = function (arr) {
+        return arr.join('-');
+    }
+    let getNewStateName = function (oldStateName, newGroups) {
+        if (groups['individualStates'].includes(oldStateName)) {
+            return oldStateName;
+        } else if (groups['queryStates'].includes(oldStateName)) {
+            return stateNameGen(groups['queryStates']);
+        } else if (groups['acceptStates'].includes(oldStateName)) {
+            return stateNameGen(groups['acceptStates']);
+        } else if (groups['individualAcceptStates'].includes(oldStateName)) {
+            return oldStateName;
+        }
+    }
+    // prepopulate the k=0 generation
+    let groups = constructGroupObj();
+    let states = dfa.states();
+
+
+    states.forEach(function (state) {
+        // console.warn("current state " + state);
+        if (dfa.acceptStates.includes(state)) {
+            // List of accept states
+            groups['acceptStates'].push(state);
+        } else {
+            groups['queryStates'].push(state);
+        }
+    });
+
+    // we grab a state from query state
+    // we check transitions and make sure both transitions end up in the same "queryStates group"
+    // If we do, we move onto the next queryState state and do the same thing
+    // If not, we remove that state from constructGroupObj,
+    // put it into individualStates and keep doing that until we don't remove from the queryStates
+
+    let cont = false;
+    do {
+        cont = false;
+        for (let i = 0; i < groups['queryStates'].length; i++) {
+            let stateToCheck = groups['queryStates'][i];
+            let stateTransitionsTo = [];
+            let alphabet = [...dfa.alphabet()];
+
+            for (let j = 0; j < alphabet.length; j++) {
+                let out = dfa.transition(stateToCheck, alphabet[j]);
+                stateTransitionsTo[j] = out;
+            }
+
+            for (let j = 0; j < stateTransitionsTo.length; j++) {
+                if (!groups['queryStates'].includes(stateTransitionsTo[j])) {
+                    groups['individualStates'].push(stateToCheck);
+                    delete groups['queryStates'][i]; // sets the element to be undefined, we are still fine array length wise
+                    cont = true;
+                }
+            }
+
+        }
+    } while (cont);
+
+    // Same as above but do it with the accept states
+    cont = false;
+    do {
+        cont = false;
+        for (let i = 0; i < groups['acceptStates'].length; i++) {
+            let stateToCheck = groups['acceptStates'][i];
+            let stateTransitionsTo = [];
+            let alphabet = [...dfa.alphabet()];
+
+            for (let j = 0; j < alphabet.length; j++) {
+                let out = dfa.transition(stateToCheck, alphabet[j]);
+                stateTransitionsTo[j] = out;
+            }
+
+            for (let j = 0; j < stateTransitionsTo.length; j++) {
+                if (!groups['acceptStates'].includes(stateTransitionsTo[j])) {
+                    groups['individualAcceptStates'].push(stateToCheck);
+                    delete groups['acceptStates'][i]; // sets the element to be undefined, we are still fine array length wise
+                    cont = true;
+                }
+            }
+
+        }
+    } while (cont);
+
+    groups['queryStates'] = groups['queryStates'].filter(function (element) {
+        return element !== undefined;
+    });
+    groups['acceptStates'] = groups['acceptStates'].filter(function (element) {
+        return element !== undefined;
+    });
+    
+    // create the new dfa
+
+    let defn = {};
+
+
+    let newTransitions = {};
+    states.forEach(function (state) {
+        // let's figure out where this is transitioning
+        let dfaTransitionToStateOn0 = dfa['transitions'][state]['0'];
+        let dfaTransitionToStateOn1 = dfa['transitions'][state]['1'];
+
+        let stateFrom = getNewStateName(state, groups);
+        let stateTo0 = getNewStateName(dfaTransitionToStateOn0, groups);
+        let stateTo1 = getNewStateName(dfaTransitionToStateOn1, groups);
+
+        if (!newTransitions.hasOwnProperty(stateFrom)) {
+            newTransitions[stateFrom] = {};
+            newTransitions[stateFrom]['0'] = stateTo0;
+            newTransitions[stateFrom]['1'] = stateTo1;
+        }
+    });
+
+    let min = {
+        transitions: newTransitions,
+        startState: getNewStateName(dfa['startState'], groups),
+        acceptStates: groups['individualAcceptStates'].concat([stateNameGen(groups['acceptStates'])]),
+    };
+
+    return new DeterministicFiniteStateMachine(min);
 }
